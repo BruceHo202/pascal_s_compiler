fd4a6a12ac2da1c1da2ca66e8819dda629c60bd8
BruceHo202 <59002136+BruceHo202@users.noreply.github.com>
2021-05-14 15:00:10 +0800

    Update main.cpp

--- final/resolve/main.cpp
+++ final/resolve/main.cpp
@@ -2,7 +2,10 @@
 #include"loadInfo.h"
 #include"resolve.h"
 
-
+void init_info(){
+    memset(mmMark, 0, sizeof(mmMark));
+    memset(data_space, 0, sizeof(data_space));
+}
 
 int main() {
     load_info();
